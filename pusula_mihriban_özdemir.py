1b5a8c0d96c5feb7f294229188b7bbe932e394c5
Mihriban Özdemir <168956501+mihribanozdemir@users.noreply.github.com>
2024-09-23 05:06:26 +0300

    pusula_mihriban_özdemir.py

--- pusula_mihriban_özdemir.py
+++ pusula_mihriban_özdemir.py
@@ -407,7 +407,14 @@ num_cols
 df.info()
 
 import pandas as pd
-
+def outlier_thresholds(dataframe, col_name, q1=0.01, q3=0.91):
+    quartile1 = dataframe[col_name].quantile(q1)
+    quartile3 = dataframe[col_name].quantile(q3)
+    interquantile_range = quartile3 - quartile1
+    up_limit = quartile3 + 1.5 * interquantile_range
+    low_limit = quartile1 - 1.5 * interquantile_range
+    return low_limit, up_limit
+    
 def check_outlier(dataframe, col_name):
     if pd.api.types.is_numeric_dtype(dataframe[col_name]):
         low_limit, up_limit = outlier_thresholds(dataframe, col_name)
